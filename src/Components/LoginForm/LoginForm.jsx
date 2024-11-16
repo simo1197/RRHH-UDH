f27893489e4e1090712cfb8da8d0e75a2bf8d6dc
GDominguez1197 <neydominguez11@gmail.com>
2024-11-16 13:20:48 -0600

    Login

--- src/Components/LoginForm/LoginForm.jsx
+++ src/Components/LoginForm/LoginForm.jsx
@@ -3,7 +3,17 @@ import './LoginForm.css';
 
 const LoginForm = () => {
     return (
-        <div>LoginForm</div>
+        <div className='wrapper'> 
+            <form action=''>
+                 <h1>Login</h1>
+                 <div className='input-box'>
+                      <input type='text' placeholder='Username' required />
+                 </div>
+                 <div className='input-box'>
+                      <input type='text' placeholder='Username' required />
+                 </div>
+            </form>
+        </div>
     );
 };
 
