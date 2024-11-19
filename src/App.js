07b00d9793d594c1e0c1a269cbf1e90453dcd8f0
jonathan-dgtUDH <jonathan.amador@udh.edu.hn>
2024-11-19 11:57:04 -0600

    Inicio de proyecto y enrutamiento

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 
 import LoginForm from './Components/LoginForm/LoginForm';
 
+//Import de para poder usar las funciones de React Router
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
+
 function App() {
   return (
     <div>
-      <LoginForm/>
+      {/* Encapsulamos todas las rutas dentro de un BrowserRouter */}
+      <BrowserRouter>
+        {/* Luego declaramos las rutas que contendra el encapsulador */}
+        <Routes>
+          
+          {/* Cada ruta singular estara dentro de Route */}
+          <Route path="/" element={<LoginForm/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
