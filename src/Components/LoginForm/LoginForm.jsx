61066afc36f28ef1ac02df9abf17e58e3b2514f2
GDominguez1197 <neydominguez11@gmail.com>
2024-11-30 14:24:48 -0600

    Cambios Login

--- src/Components/LoginForm/LoginForm.jsx
+++ src/Components/LoginForm/LoginForm.jsx
@@ -4,25 +4,29 @@ import { FaUser, FaLock } from "react-icons/fa6"; //import de icono de react
 
 const LoginForm = () => {
     return (
-        <div className='wrapper'> 
-            <form action=''>
-                 <h1>Bienvenido</h1>
-                 <div className='input-box'>
-                      <input type='text' placeholder='Usuario' required />
-                      <FaUser className='icon' /> 
-                 </div>
-                 <div className='input-box'>
-                      <input type='password' placeholder='Contraseña' required />
-                      <FaLock className='icon'/>
-                 </div>
-                 <div class="forgot-password">
-                 <a href="/forgot-password">¿Olvidaste tu contraseña?</a>
-                 </div>
-
-                 <button type='submit'>Iniciar Sesión</button>
-
-            </form>
-        </div>
+        <div class="wrapper-container">
+    <div class="image-container">
+        <img src="Assets/udhogobco.png" alt="Imagen de Login" class="login-image" />
+    </div>
+    <div class="wrapper">
+        <form action=''>
+            <h1>Bienvenido</h1>
+            <div class="input-box">
+                <input type="text" placeholder="Usuario" required />
+                <FaUser class="icon" />
+            </div>
+            <div class="input-box">
+                <input type="password" placeholder="Contraseña" required />
+                <FaLock class="icon" />
+            </div>
+            <div class="forgot-password">
+                <a href="/forgot-password">¿Olvidaste tu contraseña?</a>
+            </div>
+            <button type="submit">Iniciar Sesión</button>
+        </form>
+    </div>
+ </div>
+ 
     );
 };
 
