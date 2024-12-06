8a8cfabbd4825e1cd458ea954d3ad7bcf7d2f7af
GDominguez1197 <neydominguez11@gmail.com>
2024-12-05 20:43:09 -0600

    layout

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 //Importaciones de todas las pages a utilizar
 
-import AdminPage from './Pages/AdminPage/AdminPage';
-import UserPage from './Pages/UserPage/UserPage';
-import LoginPage from './Pages/LoginPage/LoginPage';
+import AdminPage from './pages/AdminPage/AdminPage';
+import UserPage from './pages/UserPage/UserPage';
+import LoginPage from './pages/LoginPage/LoginPage';
 
 function App() {
   return (
